df9c6bbaf2b1c96e587ab590ee19768766117549
AlinaMertina <mertinaclaudietoto@gmail.com>
2023-03-07 08:10:27 +0000

    seconde commit

--- WEB-INF/classes/FrontServlet.java
+++ WEB-INF/classes/FrontServlet.java
@@ -1,13 +1,15 @@
 package etu002087.framework.servlet;
 
+import etu002087.framework.Mapping;
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import java.lang.String;
-public class Url extends HttpServlet{
-    /**
-     * TraiteForm1
-     */
+import java.util.HashMap;
+public class FrontServlet extends HttpServlet{
+    HashMap<String,Mapping> attribue;
+    
+
     public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
         PrintWriter out = res.getWriter();
         String lien =req.getRequestURL().toString();
---
+++ WEB-INF/classes/Mapping.java
@@ -0,0 +1,11 @@
+package etu002087.framework;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.lang.String;
+public class Mapping {
+    String className;
+    String Method;
+   
+}  
