55de7c2bf006d2aa6fd7b0fe71da5c2385dd4e11
AlinaMertina <mertinaclaudietoto@gmail.com>
2023-03-11 11:48:39 +0000

    commitsprint3

---
+++ Sprint1/Emp.java
@@ -0,0 +1,13 @@
+package etu002087.framework;
+
+import etu002087.framework.Urlannotation;
+public class Emp {
+
+    public Emp(){}
+
+    @Urlannotation(index = "findAll")
+    public String findAll(){
+        return "test";
+    }
+    
+}
